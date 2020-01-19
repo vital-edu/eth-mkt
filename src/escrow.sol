13a919ff25f747cac78c3a391eba1edb13ed7e77
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2020-01-19 16:22:38 -0300

    add Approve event and tests

--- src/escrow.sol
+++ src/escrow.sol
@@ -10,4 +10,15 @@ contract EscrowContract {
     beneficiary = _beneficiary;
     depositor = msg.sender;
   }
+
+  function approve() public {
+    require(
+      msg.sender == arbiter,
+      "Only the arbiter can approve the transaction"
+    );
+    emit Approve(address(this).balance);
+    payable(beneficiary).transfer(address(this).balance);
+  }
+
+  event Approve(uint balance);
 }
