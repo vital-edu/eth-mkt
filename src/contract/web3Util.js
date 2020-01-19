b7e4f7eb4fb841eb66572b6ebbbf61347699e99a
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2020-01-19 17:14:27 -0300

    add react

--- src/contract/web3Util.js
+++ src/contract/web3Util.js
@@ -1,8 +1,8 @@
 const Web3 = require('web3');
 
 const provider = (typeof window.web3 !== 'undefined') ?
-    window.web3.currentProvider :
-    new Web3.providers.HttpProvider("http://localhost:8545");
+  window.web3.currentProvider :
+  new Web3.providers.HttpProvider("http://localhost:8545");
 
 const web3 = new Web3(provider);
 
